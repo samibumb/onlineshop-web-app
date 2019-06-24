70569c3ba538d308f35907b935c72d6699131689
Sami <bmb,sami@yahoo.com>
2019-06-24 20:34:03 +0300

    -no message-

---
+++ js/shop.js
@@ -0,0 +1,43 @@
+const API = "http://localhost:8090";
+
+window.Shop = {
+
+    getProducts: function () {
+        $.ajax({
+            url: API + "/products/getProducts",
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+            Shop.displayProducts(response.content)
+        })
+    },
+
+    getProductHtml : function (product){
+      return ` <div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="img/product-2.jpg" alt="">
+                        </div>
+                        <h2><a href="">${product.name}</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$${product.price}</ins>
+                        </div>  
+                        
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                        </div>                       
+                    </div>
+                </div>`
+    },
+
+    displayProducts:function (products) {
+        let productsHtml = "";
+
+        products.forEach(product => productsHtml += Shop.getProductHtml(product));
+
+        //css selector
+        $('#products-container').html(productsHtml)
+    }
+};
+
+Shop.getProducts();
